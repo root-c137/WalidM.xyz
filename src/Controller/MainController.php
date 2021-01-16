217980d969f780cd33c9a483ead8a19bfb0e0e29
Rootbu <mr.moussawalid@gmail.com>
2021-01-16 12:02:59 +0100

    curriculum...

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Cv;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
+        $Doc = $this->getDoctrine()->getManager();
+        $RepCV = $Doc->getRepository(Cv::class);
+        $CV = $RepCV->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'CVList' => $CV
         ]);
     }
 
